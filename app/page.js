62bc06677a5a4979f927eecc2fb88487ed5a36b0
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-05-05 13:49:47 +0530

    Added shadcn button

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <br />
+      <Button variant="secondary">World</Button>
+    </div>
+  );
 }
